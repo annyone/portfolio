3b6ba1971b3f17a202d7a4be2cbc54aa652d923a
annyone <annyone.vbg@gmail.com>
2025-05-14 22:24:18 +0300

    добавила фронтенд

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+type Project = {
+  id: number
+  name: string
+  about?: string
+  task?: string
+}
+
+export default function Home() {
+  const [projects, setProjects] = useState<Project[]>([])
+
+  useEffect(() => {
+    axios.get(`${API_URL}/api/projects`)
+      .then(res => setProjects(res.data.data))
+      .catch(err => console.error('Ошибка при загрузке:', err))
+  }, [])
+
+  return (
+    <main className="p-10">
+      <h1 className="text-2xl font-bold mb-6">Мои проекты</h1>
+      <ul className="space-y-4">
+        {projects.map(project => (
+          <li key={project.id} className="border rounded p-4 shadow">
+            <h2 className="text-xl font-semibold">{project.name}</h2>
+            {project.about && (
+              <p className="text-gray-600">{project.task}</p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
